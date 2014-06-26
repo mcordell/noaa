1087b5333dcd0ce734c9a8157cce9ad788e87c76
Michael Cordell <surpher@gmail.com>
2014-06-26 07:58:41 -0700

    Make time layout use relative refrences and handle the case of no end-times

--- lib/noaa/time_layout.rb
+++ lib/noaa/time_layout.rb
@@ -1,4 +1,4 @@
-class TimeLayout
+class Timelayout
   attr_reader :layout_key, :layout, :xml_node
 
   def from_xml_node(xml_node)
@@ -8,15 +8,19 @@ class TimeLayout
   end
 
   def get_layout_key
-    layout_key = @xml_node.xpath('//layout-key').first
+    layout_key = @xml_node.xpath('./time-layout/layout-key').first
     layout_key.text if layout_key
   end
 
   def get_layout
-    starts = @xml_node.xpath('//start-valid-time')
-    ends = @xml_node.xpath('//end-valid-time')
+    starts = @xml_node.xpath('./time-layout/start-valid-time')
+    ends = @xml_node.xpath('./time-layout/end-valid-time')
     starts.map.with_index do |start, i|
-      [DateTime.parse(start.text), DateTime.parse(ends[i].text)]
+      if (ends.empty?)
+        DateTime.parse(start.text)
+      else
+        [DateTime.parse(start.text), DateTime.parse(ends[i].text)]
+      end
     end
   end
 end
--- spec/lib/noaa/time_layout_spec.rb
+++ spec/lib/noaa/time_layout_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe TimeLayout do
+describe Timelayout do
   it { should respond_to :layout_key }
   it { should respond_to :layout }
   it { should respond_to :xml_node }
   it { should respond_to :from_xml_node }
 
-  let(:time_layout) { TimeLayout.new }
+  let(:time_layout) { Timelayout.new }
 
   describe ".from_xml_node" do
     context "when the passed xml node that contains a layout-key" do
@@ -48,6 +48,26 @@ describe TimeLayout do
       it "sets its layout to an array of DateTime pairs in the proper order" do
         expect(time_layout.layout[0][1]).to eq DateTime.parse('2014-05-01T20:00:00-07:00')
         expect(time_layout.layout[1][0]).to eq DateTime.parse('2014-05-02T08:00:00-07:00')
+      end
+    end
+    context "when the passed an xml node that contains a series of only start-valid-time" do
+      before do
+        xml = "<time-layout>
+        <start-valid-time>2014-05-01T08:00:00-07:00</start-valid-time>
+        <start-valid-time>2014-05-02T08:00:00-07:00</start-valid-time>
+        </time-layout>"
+        time_layout.from_xml_node(Nokogiri::XML(xml))
+      end
+
+      it "sets its layout to an array of DateTimes" do
+        time_layout.layout.each do |start_time,end_time|
+          expect(start_time).to be_a DateTime
+        end
+      end
+
+      it "sets its layout to an array of DateTime pairs in the proper order" do
+        expect(time_layout.layout[0]).to eq DateTime.parse('2014-05-01T08:00:00-07:00')
+        expect(time_layout.layout[1]).to eq DateTime.parse('2014-05-02T08:00:00-07:00')
       end
     end
   end
