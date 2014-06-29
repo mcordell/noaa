4aa1eb6c920b91bc8b122fa91fa5d4243399c4ea
Michael Cordell <surpher@gmail.com>
2014-06-29 13:54:19 -0700

    Better detailed forecast response spec

--- spec/lib/noaa/detailed_forecast_response_spec.rb
+++ spec/lib/noaa/detailed_forecast_response_spec.rb
@@ -31,10 +31,12 @@ describe DetailedForecastResponse do
     before { detailed_forecast_response.from_xml(File.open(detailed_data_path)) }
 
     describe ".time_layouts" do
-      it "returns a hash of time layouts" do
-        detailed_forecast_response.time_layouts.each do |k,v|
-          expect(v).to be_a Timelayout
-        end
+      it "returns a hash of hime layouts with keys being the layout keys" do
+        expected_keys = %w[k-p24h-n7-1 k-p24h-n7-2 k-p12h-n14-3 k-p6h-n7-4 k-p3h-n38-5
+                           k-p3h-n30-6 k-p3h-n22-7 k-p1h-n74-8 k-p6h-n20-9 k-p24h-n8-10
+                           k-p24h-n1-11 k-p24h-n1-12 k-p24h-n2-13 k-p6d-n1-14 k-p1m-n1-15
+                           k-p3m-n13-16 k-p24h-n7-17 k-p1h-n62-18 k-p1h-n35-19 k-p1h-n62-20]
+        expect(detailed_forecast_response.time_layouts.keys).to eq expected_keys
       end
     end
   end
